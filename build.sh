3a3dd5a7588e09278e3c36d732af5440a3d58a62
Wes Parish <wes@elastiscale.net>
2019-01-04 04:23:43 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+registry=${1:-wesparish}
+imagename=${$2:-$(basename $PWD)}
+
+for dockerfile in $(find  -name Dockerfile); do
+  versionvariant=$(dirname $dockerfile | sed -e 's|^./||g' -e 's|/|-|g')
+  echo Building variant: $versionvariant
+  echo docker build -t $registry/$versionvariant $(dirname $dockerfile)
+  docker build -t $registry/$versionvariant $(dirname $dockerfile)
+  echo docker push $registry/$versionvariant
+  docker push $registry/$versionvariant
+done
